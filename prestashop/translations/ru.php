52a7e67bd1eb823ff072ff110f7d1a9ad7de1392
Passimpay <118345040+Passimpay@users.noreply.github.com>
2025-10-29 22:40:37 +0000

    Add files via upload

---
+++ prestashop/translations/ru.php
@@ -0,0 +1,27 @@
+<?php
+
+define('PASSIMPAY_TRANSLATION_PREFIX', '<{passimpay}prestashop>passimpay_');
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE[PASSIMPAY_TRANSLATION_PREFIX . 'dc0b7dcc6715ec8cfa8298b27cd57655'] = 'Оплатить криптовалютой через Passimpay';
+$_MODULE[PASSIMPAY_TRANSLATION_PREFIX . 'fa214007826415a21a8456e3e09f999d'] = 'Вы уверены, что хотите удалить детали?';
+$_MODULE[PASSIMPAY_TRANSLATION_PREFIX . 'f0882e45de96aeffc564e1db9c10db6e'] = 'Оплатить через Passimpay';
+$_MODULE[PASSIMPAY_TRANSLATION_PREFIX . '49d3f46e1bb439e8cfe82067a7346289'] = 'Номер площадки обязателен';
+$_MODULE[PASSIMPAY_TRANSLATION_PREFIX . '7c9d0428b7071d44fc133ac6d7eb513b'] = 'Секретный ключ обязателен';
+$_MODULE[PASSIMPAY_TRANSLATION_PREFIX . 'deba6920e70615401385fe1fb5a379ec'] = 'Русский';
+$_MODULE[PASSIMPAY_TRANSLATION_PREFIX . '78463a384a5aa4fad5fa73e2f506ecfc'] = 'Английский';
+$_MODULE[PASSIMPAY_TRANSLATION_PREFIX . 'c888438d14855d7d96a2724ee9c306bd'] = 'Настройки обновлены';
+$_MODULE[PASSIMPAY_TRANSLATION_PREFIX . 'e0aa021e21dddbd6d8cecec71e9cf564'] = 'Ок';
+$_MODULE[PASSIMPAY_TRANSLATION_PREFIX . '3ec393a1685ff2b2adbf1e9459710ab7'] = 'Этот модуль позволяет производить оплату криптовалютой через Passimpay';
+$_MODULE[PASSIMPAY_TRANSLATION_PREFIX . '5dd532f0a63d89c5af0243b74732f63c'] = 'Детали';
+$_MODULE[PASSIMPAY_TRANSLATION_PREFIX . '6cbabda15cbb5a8dd550364ee1cb9693'] = 'Уточните данные';
+$_MODULE[PASSIMPAY_TRANSLATION_PREFIX . '229a7ec501323b94db7ff3157a7623c9'] = 'Терминал';
+$_MODULE[PASSIMPAY_TRANSLATION_PREFIX . '952bf87c967660b7bbd4e1eb08cefc92'] = 'Секретный ключ';
+$_MODULE[PASSIMPAY_TRANSLATION_PREFIX . '428bfc853e841c0df085fdbf545a9921'] = 'Тестовый режим';
+$_MODULE[PASSIMPAY_TRANSLATION_PREFIX . 'b17f3f4dcf653a5776792498a9b44d6a'] = 'Обновить настройки';
+$_MODULE[PASSIMPAY_TRANSLATION_PREFIX . md5('Platform details')] = 'Данные о платформе';
+$_MODULE[PASSIMPAY_TRANSLATION_PREFIX . md5('Notification URL')] = 'URL уведомлений';
+$_MODULE[PASSIMPAY_TRANSLATION_PREFIX . md5('Copy this URL and add it to your Passimpay dashboard under "Notification URL"')] = 'Скопируйте этот URL и добавьте его в панели Passimpay в поле "URL уведомлений"';
+$_MODULE[PASSIMPAY_TRANSLATION_PREFIX . md5('Platform ID')] = 'Идентификатор платформы';
+$_MODULE[PASSIMPAY_TRANSLATION_PREFIX . md5('This module allows you to accept payments by Passimpay.')] = 'Этот модуль позволяет производить оплату криптовалютой через Passimpay.';
